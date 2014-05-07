cd517aa4d782f49b81e3a1bd36ff9d3293780eb8
vinurs <haiyuan.vinurs@gmail.com>
2014-05-07 14:36:43 +0800

    update configuration for tmux

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -7,7 +7,7 @@
 # 
 #         Version:  1.0
 #         Created:  2012/08/05 10:24:48
-#         Changed:  <vinurs 02/08/2014 15:41:15>
+#         Changed:  <vinurs 05/07/2014 14:11:54>
 #        Revision:  none
 # 
 #          Author:  zhanghaiyuan
@@ -38,21 +38,21 @@ else
     # 默认打开叫sys-monitor的窗口，专门用来监控系统当前的性能的
     tmux -2 new-session -s $TMUX_SESSION -n sys-monitor \; detach-client
     # 打开文件管理器mc，我最喜欢的文件管理器了
-    tmux attach-session -t $TMUX_SESSION \; new-window -n mc \; send-keys 'mc' C-m \; detach-client
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n mc \; send-keys 'mc' C-m \; detach-client
 
-    # 为dropbox专门打开一个
-    tmux attach-session -t $TMUX_SESSION \; new-window -n dropbox \; split-window -h \; detach-client
-    # 用来挂载U盘用
-    tmux attach-session -t $TMUX_SESSION \; new-window -n mnt \; split-window -h \; detach-client
-    # 专门用来配置系统，例如一些脚本之类的
-    # 系统配置的一些东西，然后分屏，一个配置一个看效果
-    tmux attach-session -t $TMUX_SESSION \; new-window -n sys-cfg \; split-window -h \; detach-client
-    # 专门用来配置zsh
-    tmux attach-session -t $TMUX_SESSION \; new-window -n zsh-cfg \; split-window -h \; detach-client
-    # 专门用来分析内核
-    tmux attach-session -t $TMUX_SESSION \; new-window -n kernel-analyse \; send-keys 'cd ~/linux-src-study' C-m \; split-window -h \; detach-client
-    # wiki
-    tmux attach-session -t $TMUX_SESSION \; new-window -n wiki \; send-keys 'cd ~/vinurs.wiki' C-m \; split-window -h \; detach-client
+    # # 为dropbox专门打开一个
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n dropbox \; split-window -h \; detach-client
+    # # 用来挂载U盘用
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n mnt \; split-window -h \; detach-client
+    # # 专门用来配置系统，例如一些脚本之类的
+    # # 系统配置的一些东西，然后分屏，一个配置一个看效果
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n sys-cfg \; split-window -h \; detach-client
+    # # 专门用来配置zsh
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n zsh-cfg \; split-window -h \; detach-client
+    # # 专门用来分析内核
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n kernel-analyse \; send-keys 'cd ~/linux-src-study' C-m \; split-window -h \; detach-client
+    # # wiki
+    # tmux attach-session -t $TMUX_SESSION \; new-window -n wiki \; send-keys 'cd ~/vinurs.wiki' C-m \; split-window -h \; detach-client
     
     # 我自己认为用linux的时候还是命令行用得比较多，就没有必要用mc了，自己写了一个vinurs-open可以
     # 从命令行打开大多数文件，接下来就是补全它的打开功能了，主要mc的键绑定很不喜欢，配置的语法也觉得很蛋疼
