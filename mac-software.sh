0ff9ae43e44abaa132d8a236aaf54f1b83552f77
vinurs <haiyuan.vinurs@gmail.com>
2014-11-17 20:02:54 +0800

    add hexo for github blog

--- mac-software.sh
+++ mac-software.sh
@@ -7,7 +7,7 @@
 # 
 #         Version:  1.0
 #         Created:  2014/05/05 19:27:22
-#         Changed:  <vinurs 11/17/2014 19:54:07>
+#         Changed:  <vinurs 11/17/2014 20:02:36>
 #        Revision:  none
 # 
 #          Author:  zhanghaiyuan
@@ -69,3 +69,4 @@ brew install gpg
 
 # github blog
 brew install npm
+npm install -g hexo
