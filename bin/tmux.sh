099bf26be0f90695c5e25a0924aeda2b841a0d6a
zhanghaiyuan <haiyuan.vinurs@gmail.com>
2015-07-31 15:18:48 +0800

    update

--- bin/tmux.sh
+++ bin/tmux.sh
@@ -46,54 +46,13 @@ else
     tmux attach-session -t $TMUX_SESSION \; new-window -n emacs \; split-window -h \; detach-client
     # git
     tmux attach-session -t $TMUX_SESSION \; new-window -n git \; split-window -h \; detach-client
-    # octeon
-    tmux attach-session -t $TMUX_SESSION \; new-window -n octeon \; split-window -h \; detach-client
     # blog
     tmux attach-session -t $TMUX_SESSION \; new-window -n blog \; split-window -h \; detach-client
     # lisp
     tmux attach-session -t $TMUX_SESSION \; new-window -n lisp \; split-window -h \; detach-client
     # other
-	tmux attach-session -t $TMUX_SESSION \; new-window -n minicom \; detach-client
-	tmux attach-session -t $TMUX_SESSION \; new-window -n minicom \; detach-client
-	tmux attach-session -t $TMUX_SESSION \; new-window -n compile \; detach-client
-	tmux attach-session -t $TMUX_SESSION \; new-window -n src \; detach-client
-
-	tmux attach-session -t $TMUX_SESSION \; new-window  \; detach-client
-	tmux attach-session -t $TMUX_SESSION \; new-window  \; detach-client
-	tmux attach-session -t $TMUX_SESSION \; new-window  \; detach-client
+    tmux attach-session -t $TMUX_SESSION \; new-window  \; detach-client
 	
-
-    # 打开文件管理器mc，我最喜欢的文件管理器了
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n mc \; send-keys 'mc' C-m \; detach-client
-
-    # # 为dropbox专门打开一个
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n dropbox \; split-window -h \; detach-client
-    # # 用来挂载U盘用
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n mnt \; split-window -h \; detach-client
-    # # 专门用来配置系统，例如一些脚本之类的
-    # # 系统配置的一些东西，然后分屏，一个配置一个看效果
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n sys-cfg \; split-window -h \; detach-client
-    # # 专门用来配置zsh
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n zsh-cfg \; split-window -h \; detach-client
-    # # 专门用来分析内核
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n kernel-analyse \; send-keys 'cd ~/linux-src-study' C-m \; split-window -h \; detach-client
-    # # wiki
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n wiki \; send-keys 'cd ~/vinurs.wiki' C-m \; split-window -h \; detach-client
-    # emacs，这是专门为mac而定的，主要为了获取里面的PATH路径
-   # 我自己认为用linux的时候还是命令行用得比较多，就没有必要用mc了，自己写了一个vinurs-open可以
-    # 从命令行打开大多数文件，接下来就是补全它的打开功能了，主要mc的键绑定很不喜欢，配置的语法也觉得很蛋疼
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n mc \; detach-client
-
-    # 打开一个专门的terminal用来练习lisp
-    # tmux attach-session -t $TMUX_SESSION \; new-window -n lisp \; split-window -h \; detach-client
-
-
-    # 结尾的一部分window，公用的
-    # 一次多创建几个window，省得以后再创建
-    #tmux attach-session -t $TMUX_SESSION \; new-window
-    # emacs
-    # tmux attach-session -t $TMUX_SESSION \; new-window \; send-keys 'test "$system_type"x = "mac"x && emacs' C-m \; send-keys 'exit' C-m \; detach-client
-    # tmux attach-session -t $TMUX_SESSION \; new-window \; send-keys 'test "$system_type"x = "Darwin"x && emacs' C-m \; detach-client
 
     tmux attach-session -t $TMUX_SESSION \; new-window 
  
