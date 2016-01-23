8921896d1ff12d2eae57e56f338dafee68f2767b
zhanghaiyuan <haiyuan.vinurs@gmail.com>
2016-01-23 21:50:04 +0800

    update

---
+++ .emacs.d/el-get-init-packages/init-ace-window.el
@@ -0,0 +1,52 @@
+;;; init-ace-window.el --- 
+
+;; Copyright 2016 zhanghaiyuan
+;;
+;; Author: haiyuan.vinurs@gmail.com
+;; Version: $Id: @(#)init-ace-window.el,v 0.0 2016/01/23 21:40:53 vinurs Exp $
+;; Changed: <vinurs 01/23/2016 21:46:59>
+;; Keywords: 
+;; X-URL: not distributed yet
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; if not, write to the Free Software
+;; Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; Commentary:
+
+;; 
+
+;; Put this file into your load-path and the following into your ~/.emacs:
+;;   (require 'init-ace-window)
+
+
+
+
+;;; Code:
+
+(eval-when-compile
+  (require 'cl))
+
+
+
+;;;;##########################################################################
+;;;;  User Options, Variables
+;;;;##########################################################################
+
+
+(global-set-key (kbd "C-o") 'ace-window)
+
+
+(provide 'init-ace-window)
+
+;;; init-ace-window.el ends here
