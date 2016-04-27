0078ee88d7d85cee1bda32bcafe290b4828921e7
zhanghaiyuan <haiyuan.vinurs@gmail.com>
2016-04-27 10:54:43 +0800

    add basic layer

---
+++ .spacemacs.d/layers/vinurs-dev-env/keybindings.el
@@ -0,0 +1,11 @@
+(when (spacemacs/system-is-mac)
+  ;; key bindings
+  ;; for emacs
+  (setq mac-right-option-modifier 'control)
+  (setq mac-left-option-modifier 'alt)
+
+  (setq mac-command-modifier 'meta)
+
+
+  (global-set-key [kp-delete] 'delete-char) ;; sets fn-delete to be right-delete
+  )
