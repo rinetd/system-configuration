b14a66bed5ae3695a0ba5b730ca9fc18c3fa2d94
zhanghaiyuan <haiyuan.vinurs@gmail.com>
2016-05-01 08:58:33 +0800

    update

--- .spacemacs.d/layers/vinurs-basic/packages.el
+++ .spacemacs.d/layers/vinurs-basic/packages.el
@@ -32,8 +32,6 @@
 (defconst vinurs-basic-packages
   '(
     (general :location local)
-    ;; 更够用鼠标更加平滑地移动
-    smooth-scrolling
     ;; 保存上次打开的文件
     )
   "The list of Lisp packages required by the vinurs-basic layer.
@@ -63,46 +61,8 @@ Each entry is either:
       - A list beginning with the symbol `recipe' is a melpa
         recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
 
-(defun vinurs-basic/init-smooth-scrolling ()
-  (use-package smooth-scrolling
-    :defer t
-    :init
-    (progn
-      ;;(setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
-
-      ;;(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
-
-      ;;(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
-
-      ;;(setq scroll-step 2) ;; keyboard scroll one line at a time
-      ;; evil-insert模式下面c-h该为向前删除
-      (define-key evil-insert-state-map (kbd "C-h") 'delete-backward-char)
-      ;;(global-set-key [(control h)] 'delete-backward-char)
-
-      ;;;设置标题栏显示文件的完整路径名
-      (setq frame-title-format  
-            '("vinurs@知行合一^-^--PATH:%S" (buffer-file-name "%f"  
-                                                              (dired-directory dired-directory "%b"))))
-
-      ;; 保存上次打开的文件的位置
-      (save-place-mode 1)
-      ;; 保存上次打开了哪些文档
-      (desktop-save-mode 1)
-      ;; 退出的时候保存上次的光标位置
-      (setq-default save-place t)
-      ;;设置 .emacs-places 文件的存储路径
-      (setq save-place-file "~/.spacemacs.d/.emacs-places")
-
-      ;; use only one desktop
-      (desktop-save-mode 1)
-      (setq desktop-save t)
-      (setq desktop-load-locked-desktop t)
-      (setq *desktop-dir* (list (expand-file-name "~/.spacemacs.d/desktop/")))
-      (setq desktop-path *desktop-dir*)
-      (setq desktop-dir *desktop-dir*)
-      (setq desktop-base-file-name ".emacs.desktop")
-
-      )))
+(defun vinurs-basic/init-general () 
+)
 
 
 
