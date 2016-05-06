d153db784eb0cd3a57bebbfa8d9373e2f66cb5d6
zhanghaiyuan <haiyuan.vinurs@gmail.com>
2016-05-06 08:20:16 +0800

    update semantic

--- .spacemacs.d/layers/vinurs-dev-env/config.el
+++ .spacemacs.d/layers/vinurs-dev-env/config.el
@@ -1,3 +1,5 @@
 (push "/Users/vinurs/system-configuration/.spacemacs.d/layers/vinurs-dev-env/" load-path)
 ;; 映射c-h为delete
 ;; (define-key helm-map (kbd "C-h") 'delete-backward-char)
+;; (semantic-mode 1)
+;; (spacemacs/toggle-semantic-stickyfunc-globally-on)
