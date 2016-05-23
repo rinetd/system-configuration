8198bec2f87d2e656e4c2c25c17b8d3cca6a1482
zhanghaiyuan <haiyuan.vinurs@gmail.com>
2016-05-23 21:43:48 +0800

    rm

--- .spacemacs.d/layers/vinurs-basic/local/vinurs-theme/vinurs-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "tango-plus-theme" "20140425.1511" "A color theme based on the tango palette" 'nil :url "https://github.com/tmalsburg/tango-plus-theme")
