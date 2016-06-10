ec875123d026a8cc97f7090913022d9be5356bfc
vinurs <vinurs@vinurs-mac.local>
2016-06-11 00:27:34 +0800

    add company route for vpn

---
+++ bin/company-route-add.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#==============================================================================
+#
+#        Filename:  @(#)company-route-add.sh
+#
+#     Description:   add route for company vpn
+# 
+#         Version:  1.0
+#         Created:  2016/06/11 00:25:23
+#         Changed:  < >
+#        Revision:  none
+# 
+#          Author:  vinurs
+#           Email:  <haiyuan.vinurs@gmail.com>
+# 
+#==============================================================================
+##
+
+
+
+#  get ppp0's ip
+
+# add route for internal ip
+# route add 192.168.100.0/24 192.168.100.12
+
+
+exit 0
