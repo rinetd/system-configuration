9936e517337b488c69388bfb7c97286178903c43
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2016-07-06 09:20:04 +0800

    update scripts

--- dropbox-files.sh
+++ dropbox-files.sh
@@ -7,7 +7,7 @@
 # 
 #         Version:  1.0
 #         Created:  2016/07/06 07:28:31
-#         Changed:  <vinurs 07/06/2016 07:53:56>
+#         Changed:  <vinurs 07/06/2016 09:19:47>
 #        Revision:  none
 # 
 #          Author:  vinurs
@@ -22,9 +22,16 @@ if [ -d $HOME/"Dropbox/home/.getmail" ]; then
 	ln -s ~/Dropbox/home/.getmail/getmail\.*  ~/.getmail/
 fi
 
-
+# sendmail账号配置
 if [ -f $HOME/"Dropbox/home/.msmtprc"]; then
 	ln -s ~/Dropbox/home/.msmtprc    ~/
 fi
+
+# mutt通讯录
+if [ -f $HOME/"Dropbox/home/.mutt/mutt.alias"]; then
+	ln -s ~/Dropbox/home/mutt.alias  ~/.mutt/
+fi
+
+
 
 exit 0
