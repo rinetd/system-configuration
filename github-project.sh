174ee016aa8a818b1a4688506b7fc17f04cdbd2c
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2016-08-03 19:11:37 +0800

    refactor: add env file

--- github-project.sh
+++ github-project.sh
@@ -7,7 +7,7 @@
 # 
 #         Version:  1.0
 #         Created:  2016/08/03 18:36:29
-#         Changed:  <vinurs 08/03/2016 18:44:57>
+#         Changed:  <vinurs 08/03/2016 19:54:39>
 #        Revision:  none
 # 
 #          Author:  haiyuan.zhang
@@ -20,11 +20,15 @@
 github_dir=~/system-configuration/github/
 cd $github_dir
 
+# 系统配置文件的wiki
+git clone https://github.com/vinurs/system-configuration.wiki.git
 
 # my website
 git clone https://github.com/vinurs/website.git
 # 个人blog的主题
 git clone https://github.com/vinurs/hexo-theme-yelee.git website/themes/yelee
+# 用org-mode来配合hexo写blog
+git clone https://github.com/vinurs/hexo-renderer-org.git
 
 # aria2的浏览器插件，可以在浏览器里面查看aria2下载的情况
 git clone https://github.com/ziahamza/webui-aria2.git
