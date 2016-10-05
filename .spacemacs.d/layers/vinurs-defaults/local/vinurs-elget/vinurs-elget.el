3f2585d1e975d2d8eb016ee4dceae7473f86434b
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2016-10-05 09:19:36 +0800

    fix: customize el-get

--- .spacemacs.d/layers/vinurs-defaults/local/vinurs-elget/vinurs-elget.el
+++ .spacemacs.d/layers/vinurs-defaults/local/vinurs-elget/vinurs-elget.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: haiyuan.vinurs@gmail.com
 ;; Version: $Id: @(#)vinurs-elget.el,v 0.0 2016/07/28 20:41:24 vinurs Exp $
-;; Changed: <vinurs 10/04/2016 21:43:01>
+;; Changed: <vinurs 10/05/2016 09:11:53>
 ;; Keywords: 
 ;; X-URL: not distributed yet
 
@@ -47,7 +47,6 @@
 ;; 设置el-get的init-packages路径
 ;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (add-to-list 'load-path "~/.spacemacs.d/el-get/el-get")
-(setq el-get-user-package-directory "~/.spacemacs.d/el-get-init-packages")
 
 ;; 每次启动el-get的时候检测el-get是否已经安装，如果没有安装就自动安装
 (unless (require 'el-get nil 'noerror)
@@ -59,8 +58,8 @@
   ;; 将el-get安装在.spacemacs.d目录下
   (setq user-emacs-directory "~/.spacemacs.d/") 
   (package-install 'el-get) 
-  ;; 恢复user-emacs-directory
   (require 'el-get)
+  ;; 恢复user-emacs-directory
   (setq user-emacs-directory "~/.emacs.d/") 
   )
  
@@ -83,20 +82,20 @@
       )
 
 
+;; 设置一些可选参数
+(setq el-get-dir "~/.spacemacs.d/el-get")
+(setq el-get-recipe-path-elpa
+  "~/.spacemacs.d/el-get/el-get/recipes/elpa/")
+(setq el-get-recipe-path-emacswiki
+  "~/.spacemacs.d/el-get/el-get/recipes/emacswiki/")
+(setq el-get-status-file
+  "~/.spacemacs.d/el-get/.status.el")
+(setq el-get-user-package-directory
+  "~/.spacemacs.d/el-get-init-packages")
 
 
 (progn
   (setq user-emacs-directory "~/.spacemacs.d/") 
-  (setq el-get-dir "~/.spacemacs.d/el-get")
-  (setq el-get-recipe-path-elpa
-	"~/.spacemacs.d/el-get/el-get/recipes/elpa/")
-  (setq el-get-recipe-path-emacswiki
-	 "~/.spacemacs.d/el-get/el-get/recipes/emacswiki/")
-  (setq el-get-status-file
-	"~/.spacemacs.d/el-get/.status.el")
-  (setq el-get-user-package-directory
-	"~/.spacemacs.d/el-get-init-packages")
-
   (el-get 'sync)
   (el-get 'sync my-el-get-packages)
   (setq user-emacs-directory "~/.emacs.d/") 
