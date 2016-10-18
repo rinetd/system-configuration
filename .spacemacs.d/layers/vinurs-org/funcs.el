6cca63e8ea10f48053bf9f36e074ec45202d9a24
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2016-10-18 09:47:08 +0800

    feat: update emacs org-mode

--- .spacemacs.d/layers/vinurs-org/funcs.el
+++ .spacemacs.d/layers/vinurs-org/funcs.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: haiyuan.vinurs@gmail.com
 ;; Version: $Id: @(#)funcs.el,v 0.0 2016/08/31 09:50:13 vinurs Exp $
-;; Changed: <vinurs 08/31/2016 10:15:29>
+;; Changed: <vinurs 10/18/2016 09:18:59>
 ;; Keywords: 
 ;; X-URL: not distributed yet
 
@@ -72,6 +72,63 @@
 							  "i s" 'org-insert-src-block) 
 
 							)) 
+
+;; 单独设置orgtable的字体，为了中英文表格能够对齐
+;; (custom-set-faces
+;;   ;; custom-set-faces was added by Custom.
+;;   ;; If you edit it by hand, you could mess it up, so be careful.
+;;   ;; Your init file should contain only one such instance.
+;;   ;; If there is more than one, they won't work right.
+;;   '(org-table ((t (:foreground "#6c71c4" :family "Ubuntu Mono")))))
+
+;; (defvar emacs-english-font "Monaco"
+;;   "The font name of English.") 
+
+;; (defvar emacs-cjk-font "Hiragino Sans GB W3"
+;;   "The font name for CJK.") 
+
+;; (defvar emacs-font-size-pair '(15 . 18)
+;;   "Default font size pair for (english . chinese)") 
+;; (defvar emacs-font-size-pair-list
+;;   '(( 5 .  6) (10 . 12)
+;; 	 (13 . 16) (15 . 18) (17 . 20)
+;; 	 (19 . 22) (20 . 24) (21 . 26)
+;; 	 (24 . 28) (26 . 32) (28 . 34)
+;; 	 (30 . 36) (34 . 40) (36 . 44))
+;;   "This list is used to store matching (englis . chinese) font-size.") 
+
+;; (defun font-exist-p (fontname)
+;;   "Test if this font is exist or not."
+;;   (if (or (not fontname) (string= fontname ""))
+;; 	nil
+;;     (if (not (x-list-fonts fontname)) nil t))) 
+;; (defun set-font (english chinese size-pair)
+;;   "Setup emacs English and Chinese font on x window-system."
+
+;;   (if (font-exist-p english)
+;; 	(set-frame-font (format "%s:pixelsize=%d" english (car size-pair)) t))
+
+;;   (if (font-exist-p chinese)
+;; 	(dolist (charset '(kana han symbol cjk-misc bopomofo))
+;; 	  (set-fontset-font (frame-parameter nil 'font) charset
+;; 		(font-spec :family chinese :size (cdr size-pair)))))) 
+
+;; ;; Setup font size based on emacs-font-size-pair
+;; (set-font emacs-english-font emacs-cjk-font emacs-font-size-pair) 
+
+
+;; (setq fonts
+;;   (cond ((eq system-type 'darwin)     '("Monaco"    "STHeiti"))
+;; 	((eq system-type 'gnu/linux)  '("Menlo"     "WenQuanYi Zen Hei"))
+;; 	((eq system-type 'windows-nt) '("Consolas"  "Microsoft Yahei")))) 
+;; (set-face-attribute 'default nil :font
+;;   (format "%s:pixelsize=%d" (car fonts) 12)) 
+;; (dolist (charset '(kana han symbol cjk-misc bopomofo))
+;;   (set-fontset-font (frame-parameter nil 'font) charset
+;; 	(font-spec :family (car (cdr fonts))))) 
+;; ;; Fix chinese font width and rescale
+;; (setq face-font-rescale-alist '(("Microsoft Yahei" . 1.2) ("WenQuanYi Micro Hei Mono" . 1.2) ("STHeiti". 1.2))) 
+
 
 (provide 'funcs)
 
