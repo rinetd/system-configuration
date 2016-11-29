b8d941fd18962b74b8fc1da818791a7f1f850192
haiyuan.zhang <haiyuan.vinurs@gmail.com>
2016-11-30 05:25:07 +0800

    update cfs cfg

--- .spacemacs.d/layers/vinurs-defaults/vinurs-chinese-fonts-setup.el
+++ .spacemacs.d/layers/vinurs-defaults/vinurs-chinese-fonts-setup.el
@@ -4,7 +4,7 @@
 ;;
 ;; Author: haiyuan.vinurs@gmail.com
 ;; Version: $Id: @(#)vinurs-chinese-fonts-setup.el,v 0.0 2016/10/21 17:09:56 vinurs Exp $
-;; Changed: <vinurs 11/30/2016 05:02:40>
+;; Changed: <vinurs 11/30/2016 06:25:10>
 ;; Keywords: 
 ;; X-URL: not distributed yet
 
@@ -47,19 +47,38 @@
 ;; cfs配置文件目录
 (setq cfs-profiles-directory "~/.spacemacs.d/chinese-fonts-setup/")
 
+;; 维护的多套profile配置文件
+(setq cfs-profiles
+  '("default"
+	 "org-mode"
+	 ))
+
+
 ;; 构建 profile 文件所在的目录时，是否考虑当前的 ‘system-type’. 
 ;; 假设当前系统为 Linux, 当这个选项设置为 t 后，profile1 文件的路径，
 ;; 将从 ’DIR/profile1.el’ 转为 ’DIR/SYSTEM-TYPE/profile.el’
 (setq cfs-use-system-type t)
+
+
 ;; 让 chinese-fonts-setup 随着 emacs 自动生效。
 (chinese-fonts-setup-enable) 
+
+;; 定义我自己的字体
+(setq cfs-personal-fontnames (list 
+							   ;; 中文字体
+							   (font-family-list) 
+							   ;; 英文字体
+							   (font-family-list) 
+								;; 不常用汉字
+							   (font-family-list) 
+							   )) 
+;; cfs-personal-fontnames
 
 ;; 让 spacemacs mode-line 中的 Unicode 图标正确显示。
 (cfs-set-spacemacs-fallback-fonts) 
 
-
-;; (chinese-fonts-setup-enable) 
-;; (cfs-set-spacemacs-fallback-fonts) 
+;; 许多用户使用 org-mode 时，习惯让不同的标题，使用的字体大小也不同，这个特性需要用户设置：
+(setq cfs-use-face-font-rescale t)
 
 
 
